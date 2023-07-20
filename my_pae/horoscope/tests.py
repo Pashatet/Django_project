82d54f684de5cd75742ba99494764c6318685450
Pavel <92043965+Pashatet@users.noreply.github.com>
2023-07-20 22:36:59 +0300

    Add files via upload

--- my_pae/horoscope/tests.py
+++ my_pae/horoscope/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from . import views
+
 
 # Create your tests here.
+
+class TestHoroscope(TestCase):
+
+    def test_index(self):
+        response = self.client.get('/horoscope/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_libra(self):
+        response = self.client.get('/horoscope/libra/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября).',
+                      response.content.decode())
+
+    def test_libra_redirect(self):
+        response = self.client.get('/horoscope/7/')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/horoscope/libra/')
+
+    def test_zodiak_sign(self, ):
+        for i in views.signs:
+            response = self.client.get((f'/horoscope/{i}/'))
+            self.assertIn(views.signs[i], response.content.decode())
