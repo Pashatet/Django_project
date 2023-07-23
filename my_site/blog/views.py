b5adb5f10a1562c59f8da6dbd60a28bba87dc329
Pashatet <voloncevich_pavel@mail.ru>
2023-07-23 22:24:34 +0300

    v09

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -15,8 +15,14 @@ def posts(request):
 
 
 def get_info_about_post(request, name_post):
-    return HttpResponse(f'Информация о посте {name_post}')
+    data = {
+        'discription_post_name': name_post
+    }
+    return render(request, 'blog/detail_by_nam.html', context=data)
 
 
 def det_info_about_post_number(request, number_post):
-    return HttpResponse(f'Здесь содержится информация о посте под номером {number_post}')
+    data = {
+        'discription_post_number': number_post
+    }
+    return render(request, 'blog/detail_by_number.html', context=data)
