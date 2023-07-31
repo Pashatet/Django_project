7460fac952100dae17b0b865e05dd7a38535e129
Pashatet <voloncevich_pavel@mail.ru>
2023-07-31 16:57:07 +0300

    v5.3

--- movie_proj/movie_proj/settings.py
+++ movie_proj/movie_proj/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movie_app',
+    # для просомтра какие запросы отправляются к базе данных
     'django_extensions',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +51,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
 ]
 
 ROOT_URLCONF = 'movie_proj.urls'
@@ -105,6 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
+# LANGUAGE_CODE = 'ru-RU'
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
--- movie_proj/movie_proj/urls.py
+++ movie_proj/movie_proj/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'Our Admin'
+admin.site.index_title = 'Super Admin'
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('movie_app.urls')),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
