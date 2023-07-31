a1e318f25f4f60026b94093fc9b86b6d9a822b17
Pashatet <voloncevich_pavel@mail.ru>
2023-07-31 22:24:10 +0300

    v5.6

--- movie_proj/movie_app/admin.py
+++ movie_proj/movie_app/admin.py
@@ -1,16 +1,41 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Movie
+from django.db.models import QuerySet  # для анатации QuerySet
 
 
 # Register your models here.
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'rating', 'year', 'budget']
-#     добавляем пользователю редоктировать поля имя первого поля не указываем т.к он ссылочный и по нему мы переходим
+    list_display = ['name', 'rating', 'year', 'budget', 'rating_status', 'currency']
+    #     добавляем пользователю редоктировать поля имя первого поля не указываем т.к он ссылочный и по нему мы переходим
 
-    list_editable = ['rating', 'year', 'budget']
-    ordering = ['rating']
-    list_per_page = 5 # колисечтво выводимых результатов в таблице
+    list_editable = ['rating', 'year', 'budget', 'currency']
+    # ordering = ['rating']
+    list_per_page = 5  # колисечтво выводимых результатов в таблице
 
-# регистрацию бд можно сделать через декоратор
-# admin.site.register(Movie, MovieAdmin)
+    # регистрацию бд можно сделать через декоратор
+    # admin.site.register(Movie, MovieAdmin)
+
+    actions = ['ser_dollars', 'ser_euro']  # регистрируем функцию в админке
+
+    @admin.display(ordering='name', description='Status')  # метод для создания вычисляемых полей. description делает название поля автоматом берется название метода rating_status
+    def rating_status(self, movie: Movie):
+        if movie.rating < 50:
+            return 'bad'
+        if movie.rating < 70:
+            return 'good'
+        else:
+            return 'well'
+
+
+    # метод для создания дейсвий в админке (Action)
+    @admin.action(description='Установить валюту в доллар')
+    def ser_dollars(self, request, queryset: QuerySet):
+        queryset.update(currency=Movie.USD)
+
+    @admin.action(description='Установить валюту в евро')
+    def ser_euro(self, request, queryset: QuerySet):
+        count_update = queryset.update(currency=Movie.EURO)
+        self.message_user(request,
+                          f'Было обновлено {count_update} записей',
+                          messages.ERROR)
--- movie_proj/movie_app/models.py
+++ movie_proj/movie_app/models.py
@@ -14,6 +14,16 @@ class Movie(models.Model):
     year = models.IntegerField(null=True, blank=True) #blank для хранения пустых значений в поле admin
     budget = models.IntegerField(default=1000000)
     slug = models.SlugField(default='', null=False, db_index=True)
+    # Choices field https://docs.djangoproject.com/en/4.0/ref/models/fields/#field-choices применяется когда у нас маленький выбор значений
+    EURO = 'EUR'
+    USD = 'UDS'
+    RUB = 'RUB'
+    CORRENCY_CHOICES = [
+        (EURO, 'Euro'),
+        (USD, 'Dollars'),
+        (RUB, 'Rubles'),
+    ]
+    currency = models.CharField(max_length=3, choices=CORRENCY_CHOICES, default=RUB)
 
     def save(self, *args, **kwargs):
         # slug_url = translit(self.last_name + self.firs_name, language_code='ru', reversed=True)
