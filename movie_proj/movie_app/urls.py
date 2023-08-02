977621bfaf97fb1740e4960d33327f2e449a21a4
Pashatet <voloncevich_pavel@mail.ru>
2023-08-02 16:43:56 +0300

    6.3

--- movie_proj/movie_app/urls.py
+++ movie_proj/movie_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.show_all_movie),
     # path('movie/<int:id_movie>', views.show_one_movie, name='movie-detail'),
     path('movie/<slug:slug_movie>', views.show_one_movie, name='movie-detail'),
+    path('directors/', views.show_directors, name='all-directors'),
+    path('directors/<int:id_dir>', views.one_dir, name='one-directors'),
 ]
